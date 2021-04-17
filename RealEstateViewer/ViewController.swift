f74e1323860a2a5975253cb6d913a711dba41419
hnejjar <rc5973@engie.com>
2021-04-17 12:20:17 +0200

    Initial Commit

---
+++ RealEstateViewer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RealEstateViewer
+//
+//  Created by Hamza Nejjar on 17/04/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
